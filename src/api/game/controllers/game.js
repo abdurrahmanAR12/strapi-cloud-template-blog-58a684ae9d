1e7c975f875b71f941a222182ffc4c8c09b63b95
Abdur Rahman <111486128+abdurrahmanAR12@users.noreply.github.com>
2025-09-23 00:35:35 +0500

    Update game.js

--- src/api/game/controllers/game.js
+++ src/api/game/controllers/game.js
@@ -1,40 +1,9 @@
 'use strict';
 
 /**
- *  game controller
+ *  article controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::game.game', ({ strapi }) => ({
-
-  /**
-   * This brings back the default functionality for the '/games' list endpoint.
-   */
-  async find(ctx) {
-    // Calling the default core action
-    const { data, meta } = await super.find(ctx);
-    return { data, meta };
-  },
-
-  /**
-   * This is the custom logic to find a single game by its slug OR its ID.
-   * It powers the '/games/:identifier' endpoint.
-   */
-  async findOne(ctx) {
-    // 'id' is the default parameter name in Strapi's routes
-    const { id: identifier } = ctx.params;
-
-    // Delegate the lookup logic to the service
-    const entity = await strapi.service('api::game.game').findOneByIdentifier(identifier);
-
-    // If no entity is found, return a 404 error
-    if (!entity) {
-      return ctx.notFound('Game not found');
-    }
-
-    // Sanitize and transform the response
-    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
-    return this.transformResponse(sanitizedEntity);
-  }
-}));
+module.exports = createCoreController('api::game.game');
