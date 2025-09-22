40c6ce73598e76bda739ff6315827a89a2895273
Abdur Rahman <111486128+abdurrahmanAR12@users.noreply.github.com>
2025-09-23 00:38:23 +0500

    Update game.js

--- src/api/game/routes/game.js
+++ src/api/game/routes/game.js
@@ -6,4 +6,4 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::article.article');
+module.exports = createCoreRouter('api::game.game');
